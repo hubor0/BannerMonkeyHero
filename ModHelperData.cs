f91214f5b570dbd04782ebddeb0b0912db5f7e2d
hubor <minecraftcostam2009@gmail.com>
2025-02-01 16:04:07 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Banner Monkey Hero";
 
-    public const string Description = "Adds a new custom Banner Bearing Hero. complete with all 20 upgrades, art and 3d models!";
+    public const string Description = "Adds a new custom Banner Bearing Hero. complete with all 20 upgrades, art and 3d models! Note that this tower was barely tested and could be OP";
 
     public const string RepoOwner = "hubor0"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "Frankenstein-Hero"; // TODO add your repo name here, also in the download url in README.md
